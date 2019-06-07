257ba1ef12eb002065aba855d76e9c70a6c90e1f
alnusglutinosa <olga.app2@gmail.com>
2019-06-07 14:20:26 +0300

    sort file

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-// const base = './sortFile';
 const base = './src';
 const resultDir = './result';
 
 // Создаем итоговую папку
 if (!fs.existsSync(`./${resultDir}`)) {
-    fs.mkdirSync(`./${resultDir}`);
+  fs.mkdirSync(`./${resultDir}`);
 }
 
 const readDir = (base, level) => {
@@ -18,33 +17,44 @@ const readDir = (base, level) => {
     let state = fs.statSync(localBase);
 
     if (state.isDirectory()) {
-      console.log(' '.repeat(level) + 'DIR: ' + item);
       readDir(localBase, level + 1);
     } else {
 
-        //нашли файл
-        //Получаем первую букву файла
-        let dirName = item[0].toLowerCase();
+      //Нашли файл
+      //Получаем первую букву файла
+      let dirName = item[0].toLowerCase();
 
-        // Получаем папку
-        let resultBase = path.join(resultDir, dirName);
-        if (!fs.existsSync(`./${resultBase}`)) {
-            fs.mkdirSync(`./${resultBase}`);
-        }
+      // Получаем папку
+      let resultBase = path.join(resultDir, dirName);
+      if (!fs.existsSync(`./${resultBase}`)) {
+        fs.mkdirSync(`./${resultBase}`);
+      }
 
-        //Копируем туда файл
-        let resultFile = path.join(resultBase, item);
+      //Копируем туда файл
+      let resultFile = path.join(resultBase, item);
+      try {
+        fs.linkSync(localBase, resultFile);
+      } catch (err) {
+        console.log('Error copy file!');
+      }
 
-        fs.link(localBase, resultFile, err => {
-            if (err) {
-              console.error(err.message);
-              return;
-            }
-        });
-
-      //console.log(' '.repeat(level) + 'File: ' + item);
+      //Удаляем исходный файл
+      try {
+        fs.unlinkSync(localBase);
+      } catch (err) {
+        console.log('Error delete file!');
+      }
     }
-  })
-}
+  });
+
+  //Удалить папку
+  fs.rmdir(base, err => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('Delete done!');
+  });
+};
 
 readDir(base, 0);
